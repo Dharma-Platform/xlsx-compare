c20e1acc04d6cfa353b0b9dda1e56a882ecc6f3e
Bella Woo <woo.bella@gmail.com>
2017-10-26 16:24:29 -0400

    email typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='xlsx-compare',
     version='0.1.1',
     author='Bella Woo',
-    author_email='bella@dharmaai.com',
+    author_email='bella@dharmaplatform.com',
     license='BSD',
     description='A Python script to compare the equivalency of two Excel documents',
     long_description=open('README.rst').read(),
