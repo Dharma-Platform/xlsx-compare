c18d0bc85f14a375d6f38a8c04ee9498cc791853
Bella Woo <woo.bella@gmail.com>
2017-10-26 15:21:01 -0400

    logistics for pypi package registration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xlsx-compare
 Forked from XLSXwriter
+
+A package that extracts some test helper functions from XLSXwriter for external use. The script provies the ability to compare the equivalency of two Excel documents.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xlsx-compare',
+    version='0.1',
+    author='Bella Woo',
+    author_email='bella@dharmaai.com',
+    license='BSD',
+    description='A Python script to compare the equivalency of two Excel documents'
+    long_description=open('README.md').read(),
+    scripts=['xlsx-compare']
+)
