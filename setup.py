26840f5ca9142d4ae9eb2ba9f497f25c390648f5
Bella Woo <woo.bella@gmail.com>
2017-10-26 17:19:39 -0400

    update release version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='xlsx-compare',
-    version='0.1.1',
+    version='0.1.2',
     author='Bella Woo',
     author_email='bella@dharmaplatform.com',
     license='BSD',
     description='A Python script to compare the equivalency of two Excel documents',
     long_description=open('README.rst').read(),
-    scripts=['xlsx-compare']
+    py_modules=['xlsx-compare']
 )
