02a235b876b03358f4bfb297d6ca8ee2b65db042
Bella Woo <woo.bella@gmail.com>
2017-11-02 14:50:31 -0400

    rename module for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='xlsx_compare',
+    name='xlsxcompare',
     version='0.0.1',
     author='Bella Woo',
     author_email='bella@dharmaplatform.com',
